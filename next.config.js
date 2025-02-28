6c37c598bce9898c7b402ad4335e404562a0432f
neonswapdeveloper <neonswap@arvix.us>
2025-02-28 15:50:40 +0100

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['assets.coincap.io', 'cryptologos.cc', 'content-api.changenow.io', 'api.changenow.io'],
+    unoptimized: true,
   },
+  output: 'export',
+  basePath: '/neon',
+  assetPrefix: '/neon/',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig; 
